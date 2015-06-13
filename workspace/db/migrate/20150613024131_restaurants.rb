ac154a0d02edbe2728708c3a0b8544d1e8677dbb
Carlos Miranda Valencia <c_miranda_v@hotmail.com>
2015-06-13 17:32:02 +0000

    correccion de migrate restaurans - ciudad

--- workspace/db/migrate/20150613024131_restaurants.rb
+++ workspace/db/migrate/20150613024131_restaurants.rb
@@ -1,6 +1,6 @@
 class Restaurants < ActiveRecord::Migration
   def change
     add_column :restaurants,:user_id,:integer
-    add_column :restaurants,:cuidad_id,:integer
+    add_column :restaurants,:ciudad_id,:integer
   end
 end
